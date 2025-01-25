3b99be1ad7a9486d44db61a2a16d5dd5c183f447
Tejas Jagdale <thetejasjagdale@gmail.com>
2025-01-25 13:50:33 +0530

    Fix Lusitana font subsets syntax

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,7 @@ import { Inter } from "next/font/google";
 import { Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-
-export const lusitana = Lusitana({ weight: ["700"] });
+export const lusitana = Lusitana({
+  weight: ["700"],
+  subsets: ["latin"],
+});
